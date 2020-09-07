5c485b1c8931327b883dd7a7c24da880015ea173
Wall <BWall@enterprise-ireland.com>
2020-09-07 12:23:41 +0100

    Add project files.

---
+++ WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,27 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        document.getElementById("home").className.replace("active", "notactive");
+        var current = document.getElementsByClassName(" active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+
+
+//var header = document.getElementById("hordiv");
+//var btns = header.getElementsByClassName("btn");
+//for (var i = 0; i < btns.length; i++) {
+//    btns[i].addEventListener("click", function () {
+//        var current = document.getElementsByClassName("active");
+//        current[0].className = current[0].className.replace(" active", "");
+//        this.className += " active";
+//    });
+//}
